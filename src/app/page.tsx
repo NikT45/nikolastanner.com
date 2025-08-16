f37761aefe8ef92d6858c9c31ef3de321f7c5ff4
N1k0405 <nikolasethantanner@gmail.com>
2025-08-16 18:58:23 -0400

    text change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
       <div className="flex flex-col lg:flex-row items-center justify-items-center">
         <VideoCard />
         <div className="flex lg:pl-32 lg:pr-24 pt-8 lg:pt-0 flex-col items-center justify-items-center">
-          {/* <p className="text-black text-[28px] font-regular" style={{ fontFamily: '"Google Sans", Roboto, Arial, sans-serif' }}>I'm cooler in person</p> */}
-          <p className="text-[#252525] text-[28px] font-normal text-center" style={{ fontFamily: '"Google Sans", Roboto, Arial, sans-serif' }}>Wanna skip the slides?</p>
+          <p className="text-black text-[28px] font-normal text-center" style={{ fontFamily: '"Google Sans", Roboto, Arial, sans-serif' }}>I'm cooler in person</p>
+          {/* <p className="text-[#252525] text-[28px] font-normal text-center" style={{ fontFamily: '"Google Sans", Roboto, Arial, sans-serif' }}>Wanna skip the slides?</p> */}
           <p className="text-[#252525] text-[14px] font-medium mt-4" style={{ fontFamily: '"Google Sans", Roboto, Arial, sans-serif' }}>Get to know me over a call</p>
           <button 
             onClick={() => window.open('https://calendly.com/ntanner2-fordham/30min', '_blank')}
