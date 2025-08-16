205aefb0eae5c3244b26e973b8c5637fa3d03624
N1k0405 <nikolasethantanner@gmail.com>
2025-08-16 16:40:55 -0400

    video card done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import VideoCard from "@/components/VideoCard";
 
 export default function Home() {
   return (
-    <div className="font-sans flex flex-col items-center justify-items-center min-h-screen bg-white">
+    <div className="font-sans flex flex-col items-center justify-items-center min-h-screen bg-white py-12">
        <VideoCard />
      
     </div>
--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,16 +1,40 @@
+import { Ellipsis, EllipsisVertical, Mic, SquareUserRound, Video } from "lucide-react";
 
-
-export default function VideoCard() {   
+export default function VideoCard() {
     return (
         <div className="max-w-[460px] rounded-lg relative">
             <div className="w-full aspect-video rounded-lg overflow-hidden">
                 <video src="/loopedvid.mp4" className="w-full h-full object-cover rounded-lg" autoPlay loop muted />
             </div>
             {/* Dark gradient overlay */}
-            <div className="absolute inset-0 pointer-events-none rounded-lg" 
-                 style={{
-                     background: 'linear-gradient(to bottom, rgba(0,0,0,0.6) 0%, transparent 20%, transparent 80%, rgba(0,0,0,0.6) 100%)'
-                 }}>
+            <div className="absolute inset-0 pointer-events-none rounded-lg"
+                style={{
+                    background: 'linear-gradient(to bottom, rgba(0,0,0,0.6) 0%, transparent 20%, transparent 80%, rgba(0,0,0,0.6) 100%)'
+                }}>
+            </div>
+            {/* Top flex row overlay */}
+            <div className="absolute top-0 left-0 right-0 flex items-center justify-between px-4 py-4 rounded-t-lg z-10">
+                <a className="text-white text-[14px] font-medium" style={{ fontFamily: '"Google Sans", Roboto, Arial, sans-serif' }}>Nikolas Tanner</a>
+                <EllipsisVertical size={20} strokeWidth={2} className="text-white" />
+            </div>
+            {/* Bottom flex row overlay */}
+            <div className="absolute bottom-0 left-0 right-0 h-12 flex items-center justify-between rounded-b-lg z-10 p-4">
+                <div className="flex items-center justify-between bg-[#2B79E2] rounded-full p-0.5">
+                    <Ellipsis size={16} strokeWidth={2.5} className="text-white" />
+                </div>
+                <div className="flex items-center gap-4 justify-between rounded-full p-0.5">
+                    <button className="text-white border-1 border-white rounded-full p-3 text-[14px] font-medium mb-4 cursor-pointer" >
+                        <Mic size={16} strokeWidth={2.5} />
+                    </button>
+                    <button className="text-white border-1 border-white rounded-full p-3 text-[14px] font-medium mb-4 cursor-pointer" >
+                        <Video size={16} strokeWidth={2.5} />
+                    </button>
+                </div>
+                <div>
+                    <button className="text-white border-1 border-white rounded-full p-3 text-[14px] font-medium mb-4 cursor-pointer" >
+                    <SquareUserRound size={16} strokeWidth={2.5} />
+                    </button>
+                </div>
             </div>
         </div>
     )
