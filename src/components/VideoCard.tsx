95a9a29d478b7e59745a1eff62decb73a0304cad
N1k0405 <nikolasethantanner@gmail.com>
2025-08-16 16:03:24 -0400

    aspect ratio fixed

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -3,8 +3,8 @@
 export default function VideoCard() {   
     return (
         <div className="max-w-[460px] rounded-lg relative">
-            <div className="w-full h-full rounded-lg">
-                <video src="/loopedvid.mp4" width={576} height={576} className="w-full h-full object-cover rounded-lg" autoPlay loop muted />
+            <div className="w-full aspect-video rounded-lg overflow-hidden">
+                <video src="/loopedvid.mp4" className="w-full h-full object-cover rounded-lg" autoPlay loop muted />
             </div>
             {/* Dark gradient overlay */}
             <div className="absolute inset-0 pointer-events-none rounded-lg" 
