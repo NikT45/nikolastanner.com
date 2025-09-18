ac4a7d7f51ddbaabcee3e50aff6276a500ba493a
N1k0405 <nikolasethantanner@gmail.com>
2025-09-18 11:27:57 -0400

    dither added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
 import HomePage from "@/components/HomePage";
-
+import Dither from "@/components/Dither";
 export default function Home() {
-  return <HomePage />;
+  return (
+    <div className="absolute z-0 inset-0 h-full w-full">
+      <Dither
+        waveColor={[0, 0, 1]}
+        disableAnimation={false}
+        enableMouseInteraction={false}
+        mouseRadius={0.3}
+        colorNum={4}
+        waveAmplitude={0.3}
+        waveFrequency={3}
+        waveSpeed={0.01}
+        opacity={0.2}
+      />
+    </div>
+    /*<HomePage />*/
+  );
 }
